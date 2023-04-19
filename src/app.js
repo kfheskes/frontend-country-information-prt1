e7a2f0c5e508905a8017dba27e4d6096924bd448
kfheskes <kayarneheskes@hotmail.com>
2023-04-19 20:01:12 +0200

    uitwerkingen19april2023

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
+import axios from "axios";
+
+async function fechtCountries (){
+    try {
+        const response = await axios.get('https://restcountries.com/v3.1/all?')
+        console.log(response.data)
+    }
+    catch (e){
+        console.error(e)
+    }
+}
+
+void fechtCountries();
+
+
+
 console.log('Hallo daar!');
